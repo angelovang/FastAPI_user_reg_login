589df9a37710956fd45c810f3048a1d51c02a79e
angelovang <angelovang55@gmail.com>
2025-09-26 18:14:43 +0300

    CRUD USER With column "role", add alembic migration

--- backend/crud.py
+++ backend/crud.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Session
 from . import models, schemas
 from typing import Optional
 
+
 def authenticate_user(db: Session, username: str, password: str) -> Optional[models.User]:
     """Връща потребителя, ако username и password са валидни, иначе None"""
     user = db.query(models.User).filter(models.User.username == username).first()
@@ -11,22 +12,29 @@ def authenticate_user(db: Session, username: str, password: str) -> Optional[mod
         return None
     return user
 
+
 def get_user_by_username(db: Session, username: str):
     return db.query(models.User).filter(models.User.username == username).first()
+
 
 def get_user_by_email(db: Session, email: str):
     return db.query(models.User).filter(models.User.email == email).first()
 
+
 def create_user(db: Session, user: schemas.UserCreate):
-    db_user = models.User(username=user.username, email=user.email)
+    db_user = models.User(username=user.username,
+                          email=user.email,
+                          role=user.role or "user")
     db_user.set_password(user.password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
 
+
 def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
+
 
 def update_user(db: Session, user_id: int, user: schemas.UserBase):
     db_user = db.query(models.User).filter(models.User.id == user_id).first()
@@ -36,6 +44,7 @@ def update_user(db: Session, user_id: int, user: schemas.UserBase):
         db.commit()
         db.refresh(db_user)
     return db_user
+
 
 def delete_user(db: Session, user_id: int):
     db_user = db.query(models.User).filter(models.User.id == user_id).first()
--- backend/schemas.py
+++ backend/schemas.py
@@ -1,20 +1,19 @@
+from typing import Optional
+
 from pydantic import BaseModel, EmailStr, constr
 
 
 class UserBase(BaseModel):
     username: str
     email: str
+    role: Optional[str] = "user"
 
-#class UserLogin(BaseModel):
-#    username: str
-#    password: str
 
 class UserLogin(BaseModel):
     username: constr(min_length=3, max_length=50)
     password: constr(min_length=4)
 
-#class UserCreate(UserBase):
-#    password: str
+
 class UserCreate(BaseModel):
     username: constr(min_length=3, max_length=50)
     email: EmailStr
